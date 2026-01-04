7391f4a224cdc4fea6338a82a80244a035417804
SkoogyTV <seanskaugen@proton.me>
2026-01-04 17:30:40 -0600

    The main function has a usuage page and can track -nand -f

---
+++ src/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <getopt.h>
+
+#include "common.h"
+#include "file.h"
+#include "parse.h"
+
+void print_usage(char *argv[]){
+	printf("Usage: %s -n -f <database file>\n", argv[0]);
+	printf("\t -n - create new database file\n");
+	printf("\t -f - (required) path to database file\n");
+	return;
+}
+
+int main(int argc, char *argv[]){
+	char *filepath = NULL;
+	bool newfile = false;
+	int c;
+
+	while((c = getopt(argc, argv, "nf:")) != -1){
+		switch(c){
+			case 'n':
+				newfile = true;
+				break;
+			case 'f':
+				filepath = optarg;
+				break;
+			case '?':
+				printf("Unknown Option -%c\n",c);
+			default:
+				//Something went wrong should not ever end up here
+				return -2;
+		}
+	}
+	if (filepath == NULL){
+		print_usage(argv);
+	}
+	printf("NewFile: %d\n", newfile);
+	printf("FilePath: %s\n", filepath);
+	return 0;
+}
